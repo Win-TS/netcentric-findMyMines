7872fd53f6b66685955ce2a690f61c7dc08cfab4
mChitipat <mchitipat@gmail.com>
2023-11-09 20:51:06 +0700

    instruction css

--- frontend/src/pages/HowToPlay.js
+++ frontend/src/pages/HowToPlay.js
@@ -3,33 +3,43 @@ import goldenBombImg from "../assets/goldenBomb.png";
 
 const HowToPlay = () => {
   return (
-    <div className="howtoplay-container">
-      <h1>How to Play</h1>
-      <p>Here are the instructions on how to play the game:</p>
+    <div className="how-to-play-container">
+      <h1 className="how-to-play">How to Play</h1>
+      <p className="instructions-text">
+        Here are the instructions on how to play the game:
+      </p>
       <ol>
-        <li>
-          In this game, you will have to compete to find mines with your peer.
-        </li>
+        <li>In this game, you will compete to find mines with your peer.</li>
         <li>First, choose the difficulty.</li>
-        <li>Enter a game room, or create a new one.</li>
+        <li>Enter a game room or create a new one.</li>
         <li>
-          Once in the game, player will be randomly start and you will each have
-          10 secs per round
+          Once in the game, a player will be randomly chosen,
+          <br /> and you will each have 10 seconds per round.
         </li>
         <ul>
           <li>If you reveal a mine, you receive 1 point!</li>
           <li>If you reveal a golden mine, you receive 3 points!</li>
-          <img src={bombImg} width="60px" height="60px" />
-          <img src={goldenBombImg} width="50px" height="50px" />
+          <div className="mine-images">
+            <img src={bombImg} alt="Bomb" className="bomb-image" />
+            <img
+              src={goldenBombImg}
+              alt="Golden Bomb"
+              className="golden-bomb-image"
+            />
+          </div>
           <li>
             Extra! If you find 3 streak mines, 1 bonus point will be given!
           </li>
-          <li>At the end, player with the highest points will be winner.</li>
-          <li>Restart button will be on; but winner will start first!</li>
+          <li>
+            At the end, the player with the highest points will be the winner.
+          </li>
+          <li>
+            Restart button will be available, but the winner will start first!
+          </li>
         </ul>
-        <li>Try to use strategic move to win this game!</li>
+        <li>Try to use strategic moves to win this game!</li>
       </ol>
-      <p>Enjoy the game and good luck!</p>
+      <p className="good-luck-text">Enjoy the game and good luck!</p>
     </div>
   );
 };
